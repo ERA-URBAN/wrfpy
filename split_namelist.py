50f6e24ddbdf865f46678c060f54aed95683c52b
Ronald van Haren <r.vanharen@esciencecenter.nl>
2016-02-10 16:09:15 +0100

    various bugs fixed

--- split_namelist.py
+++ split_namelist.py
@@ -9,24 +9,29 @@ author:         Ronald van Haren, NLeSC (r.vanharen@esciencecenter.nl)
 import f90nml
 import copy
 import mpl_toolkits.basemap.pyproj as pyproj 
+from config import config
 
 class split_nml_shared(config):
   '''
   shared functionality for split_nml_wrf and split_nml_wps
   '''
-  def __init__(self):
+  def __init__(self, namelist):
     config.__init__(self)
+    self.namelist = namelist
+    self.read_namelist()
+    self.create_namelist_copies()
+    self.modify_coarse_namelist()
+    self.modify_fine_namelist()
 
-  def read_namelist(self, namelist)
+  def read_namelist(self):
     '''
     read user supplied namelist
     '''
-    self.nml = f90nml.read(namelist)
+    self.nml = f90nml.read(self.namelist)
     # get list of namelist keys
-    self.keys = self.wrf_nml.keys()
+    self.keys = self.nml.keys()
 
-
-  def create_namelist_copies(self)
+  def create_namelist_copies(self):
     '''
     create two (shallow) copies of the variable containing the namelist
     which will be used to create the output namelists
@@ -57,12 +62,14 @@ class split_nml_shared(config):
     special_cases1 = ['parent_grid_ratio', 'i_parent_start', 'j_parent_start',
                      'parent_time_step_ratio']
     special_cases2 = ['grid_id', 'parent_id']
+    print self.nml['metgrid'].keys()
     for section in self.nml.keys():
       for key in self.nml[section].keys():
+        print key
         if isinstance(self.nml[section][key], list):
           if key in special_cases1:
-            if len(self.nml][section][key] > 2:
-              self.nml_fine[section][key] = 1 + self.nml[
+            if len(self.nml[section][key]) > 2:
+              self.nml_fine[section][key] = [1] + self.nml[
                 section][key][2:]
             else:
               self.nml_fine[section][key] = 1
@@ -96,8 +103,8 @@ class split_nml_wrf(split_nml_shared):
     write coarse and fine WRF namelist.input to the respective run directories
     as namelist.forecast
     '''
-    coarse_namelist_dir = self.config{'filesystem'}{'wrf_run_dir'} + '_coarse'
-    fine_namelist_dir = self.config{'filesystem'}{'wrf_run_dir'} + '_fine'
+    coarse_namelist_dir = self.config['filesystem']['wrf_run_dir'] + '_coarse'
+    fine_namelist_dir = self.config['filesystem']['wrf_run_dir'] + '_fine'
     self.nml_coarse.write(os.path.join(coarse_namelist_dir,
                                        'namelist.forecast'))
     self.nml_fine.write(os.path.join(fine_namelist_dir,
@@ -106,12 +113,11 @@ class split_nml_wrf(split_nml_shared):
 
 
 
-class split_nml_wps(split_nml_shared):
+class split_nml_wps(split_nml_shared, config):
   def __init__(self):
-    split_nml_shared.__init__(self)
-    split_nml_shared.read_namelist(wps_namelist)  # TODO: define wps_namelist
-    split_nml_shared.create_namelist_copies()
-    split_nml_shared.modify_coarse_namelist()
+    config.__init__(self)  # load config
+    wps_namelist = self.config['options_wps']['namelist.wps']
+    split_nml_shared.__init__(self, wps_namelist)
     self._modify_fine_namelist_wps()
     self._save_namelists()
 
@@ -146,10 +152,10 @@ class split_nml_wps(split_nml_shared):
     self.dx = float(self.nml['geogrid']['dx']) / grid_ratio
     self.dy = float(self.nml['geogrid']['dy']) / grid_ratio
     # define projection string
-    projstring  = ("+proj=lcc +lat_1=%s +lat_2=%s +lat_0=%s +lon_0=%s ",
-                   "+x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-                   %(truelat1, truelat2, ref_lat, ref_lon))
-    lambert = pyproj.Proj( projstring )
+    projstring  = ("+proj=lcc +lat_1=%s +lat_2=%s +lat_0=%s " 
+                   "+lon_0=%s +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+                   %(str(truelat1), str(truelat2), str(ref_lat), str(ref_lon)))
+    projection = pyproj.Proj( projstring )
     # calculate east/west/south/north
     west = (-self.nml['geogrid']['dx'] * (e_we[0] - 1) * 0.5) + (
       (i_start[1] - 1) * self.nml['geogrid']['dx'])
@@ -161,20 +167,25 @@ class split_nml_wps(split_nml_shared):
       (e_sn[1] - 1) * self.dy)
     # new ref_lat and ref_lon
     self.ref_lon, self.ref_lat = projection((west + east) * 0.5,
-                                            (north + south) * 0.5,
-                                            inverse=True )
+                                          (north + south) * 0.5,
+                                          inverse=True )
+    # TODO: fix projection, result seems wrong
 
-
+	
   def _save_namelists(self):
     '''
     write coarse and fine WRF namelist.input to the respective run directories
     as namelist.forecast
     '''
-    coarse_namelist_dir = os.path.join(self.config{'filesystem'}{'work_dir'},
+    coarse_namelist_dir = os.path.join(self.config['filesystem']['work_dir'],
                                        'wps_coarse')
-    fine_namelist_dir = os.path.join(self.config{'filesystem'}{'work_dir'},
+    fine_namelist_dir = os.path.join(self.config['filesystem']['work_dir'],
                                        'wps_fine')
     self.nml_coarse.write(os.path.join(coarse_namelist_dir,
                                            'namelist.wps'))
     self.nml_fine.write(os.path.join(fine_namelist_dir,
                                          'namelist.wps'))
+
+
+if __name__=="__main__":
+  split_nml_wps()
