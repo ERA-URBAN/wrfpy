2009b5b1eb20eb0e979fb07aea90a513dbb5ef32
Ronald van Haren <R.vanHaren@esciencecenter.nl>
2018-06-19 11:02:45 +0200

    split cylc configuration creation from run script

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "WRFpy",
-    version = "0.0.1",
+    version = "0.1.0",
     author = "Ronald van Haren",
     author_email = "r.vanharen@esciencecenter.nl",
     description = ("A python application that provides an easy way to set up,"
@@ -19,11 +19,12 @@ setup(
     include_package_data = True,    # include everything in source control
     package_data={'wrfpy': ['cylc/*.py']},
     scripts=['wrfpy/scripts/wrfpy'],
-    long_description=read('README.md'),
+    long_description=read('README.rst'),
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved ::Apache Software License",
     ],
+    install_requires=['numpy', 'Jinja2', 'MarkupSafe', 'PyYAML', 'f90nml'],
 )
 
